0bc93db53e4d06ad4b07a8a70f96bf6d5bee2d93
ViniFarias <vinifarias.vf@gmail.com>
2019-06-24 14:21:19 -0300

    Adiciona implementação da questão 'Catraca de Ônibus'

--- fsm/catraca_onibus.sv
+++ fsm/catraca_onibus.sv
@@ -49,4 +49,81 @@ module top(input  logic clk_2,
              lcd_ALUResult, lcd_Result, lcd_WriteData, lcd_ReadData, 
            output logic lcd_MemWrite, lcd_Branch, lcd_MemtoReg, lcd_RegWrite);
 
+  //  Estados
+  parameter LIBERADA=1, TRAVADA=0;
+  
+  logic reset, passe1, passe2, catraca, state;
+  logic [1:0] carrega1, carrega2;
+  logic [2:0] conta, cartao1, cartao2;
+
+  //  Entradas
+  always_comb begin
+    reset <= SWI[6];
+    passe1 <= SWI[0];
+    passe2 <= SWI[1];
+    carrega1 <= SWI[3:2];
+    carrega2 <= SWI[5:4];
+  end
+
+  //  Execução do loop da máquina de estados
+  always_ff@(posedge clk_2 or posedge reset) begin
+    if(reset) begin
+      catraca <= 0;
+      conta = 0;
+      state <= TRAVADA;
+    end
+    else begin
+
+      if((carrega1 + cartao1) > 5) cartao1 <= 5;
+      else cartao1 <= cartao1 + carrega1;
+
+      if((carrega2 + cartao2) > 5) cartao2 <= 5;
+      else cartao2 <= cartao2 + carrega2;
+
+      unique case (state)
+
+        TRAVADA: begin
+          catraca <= 0;
+          conta = 0;
+
+          if(passe1 && passe2) state <= TRAVADA;
+          else if(passe1) begin
+            if(cartao1 > 0) begin
+              state <= LIBERADA;
+              cartao1 <= cartao1 - 1;
+            end
+          end
+          else if(passe2) begin
+            if(cartao2 > 0) begin
+              state <= LIBERADA;
+              cartao2 <= cartao2 - 1;
+            end
+          end
+        end
+
+        LIBERADA: begin
+          catraca <= 1;
+          state <= TRAVADA;
+        end
+      endcase
+    end
+  end
+
+  //  Saídas
+  always_comb begin
+    LED[0] <= catraca;
+    SEG[7] <= clk_2;
+
+    if(passe1) conta = cartao1;
+    else if(passe2) conta = cartao2;
+
+    unique case (conta)
+      0: SEG[6:0] <= 7'b0111111;
+      1: SEG[6:0] <= 7'b0000110;
+      2: SEG[6:0] <= 7'b1011011;
+      3: SEG[6:0] <= 7'b1001111;
+      4: SEG[6:0] <= 7'b1100110;
+      5: SEG[6:0] <= 7'b1101101;
+    endcase
+  end
 endmodule
